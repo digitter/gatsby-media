f56327d1e531a80fd507afa26d44b20713ebea32
kazuki <matchingserver@gmail.com>
2021-05-15 22:31:48 +0900

    improve site header width

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,10 +3,9 @@ import { Link } from 'gatsby'
 import styles from './navigation.module.css'
 
 export default () => (
-  <header style={{width: 990, margin: 'auto'}}>
-    <div style={{ display: 'flex', justifyContent: 'flex-end', marginLeft: 50 }}>
-      <h1 style={{marginRight: 'auto'}}><strong>Tech Mesia</strong></h1>
-
+  <header className={styles.header}>
+    <div className={styles.header_wrapper}>
+      <h1>Tech Mesia</h1>
       <nav role="navigation">
         <ul className={styles.navigation}>
           <li className={styles.navigationItem}>
